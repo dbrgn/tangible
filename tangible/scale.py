41a8a73fcc3694db9a180dca8a0232d099a9c547
Danilo Bargen <mail@dbrgn.ch>
2013-10-08 11:32:09 +0200

    Added linear scale function

--- tangible/scale.py
+++ tangible/scale.py
@@ -2,10 +2,39 @@
 from __future__ import print_function, division, absolute_import, unicode_literals
 
 
+def _clamp(value, domain):
+    """
+    Clamp function. Limits the value to the given domain.
+
+    (TODO: Maybe rewriting this using ``numpy.clip`` would make it faster?)
+
+    :param value: The value to clamp.
+    :param domain: A 2-tuple with the domain to clamp to.
+    :returns: The value, clamped to the specified domain.
+
+    """
+    return sorted((domain[0], value, domain[1]))[1]
+
+
 def linear(domain, codomain, clamp=False):
     """
     Return a function to linearly scale the values in the ``domain`` range to
     values in the ``codomain`` range.
 
+    :param domain: The scale's input domain.
+    :type domain: A 2-tuple.
+    :param codomain: The scale's output range / codomain.
+    :type codomain: A 2-tuple.
+    :returns: A function that takes a single number as argument and returns
+    another number.
+
     """
-    pass
+    d, c = domain, codomain  # Short aliases
+    d_size = d[1] - d[0]
+    c_size = c[1] - c[0]
+
+    def scale(x):
+        value = (c_size / d_size) * (x - d[0]) + c[0]
+        return _clamp(value, codomain) if clamp is True else value
+
+    return scale
---
+++ tests/test_scale.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function, division, absolute_import, unicode_literals
+
+import pytest
+
+from tangible import scale
+
+
+@pytest.mark.parametrize(('param', 'clamp', 'expected'), [
+    # Regular values
+    (2, False, 10),
+    (3, False, 15),
+    (3.5, False, 17.5),
+    (4, False, 20),
+    # Clamping
+    (6, False, 30),
+    (6, True, 20),
+    (-1, False, -5),
+    (-1, True, 10),
+])
+def test_linear(param, clamp, expected):
+    """Test the linear scale."""
+    domain = (2, 4)
+    codomain = (10, 20)
+    s = scale.linear(domain, codomain, clamp)
+    assert s(param) == expected
+
+
+@pytest.mark.parametrize(('param', 'clamp', 'expected'), [
+    # Regular values
+    (1, False, 20),
+    (3, False, 10),
+    (2.5, False, 12.5),
+    # Clamping
+    (0, False, 25),
+    (0, True, 20),
+    (7, False, -10),
+    (7, True, 10),
+])
+def test_linear_inverted_codomain(param, clamp, expected):
+    """Test the linear scale with inverted codomain."""
+    domain = (1, 3)
+    codomain = (20, 10)
+    s = scale.linear(domain, codomain, clamp)
+    assert s(param) == expected
