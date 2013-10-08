162d2b614e3bc17396a569aa844bd1278ef11005
Danilo Bargen <mail@dbrgn.ch>
2013-10-08 16:11:29 +0200

    First version of AST module

---
+++ tangible/ast.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+AST module.
+
+This module contains the building blocks for an abstract syntax tree (AST)
+representation of 3D objects. It is implemented using namedtuples.
+
+"""
+from __future__ import print_function, division, absolute_import, unicode_literals
+
+from collections import namedtuple
+
+__VERSION__ = '1'
+
+
+# 2D shapes
+Circle = namedtuple('Circle', 'radius')
+Rectangle = namedtuple('Rectangle', 'width heigh center')
+Polygon = namedtuple('Polygon', 'points paths')
+
+# 3D shapes
+Sphere = namedtuple('Sphere', 'radius')
+Cube = namedtuple('Cube', 'width height depth')
+Cylinder = namedtuple('Cylinder', 'height radius1 radius2')
+Polyhedron = namedtuple('Polyhedron', 'points triangles')
+
+# Transformations
+Translate = namedtuple('Translate', 'x y z')
+Rotate = namedtuple('Rotate', 'x y z')
+Scale = namedtuple('Scale', 'x y z')
+Mirror = namedtuple('Mirror', 'x y z')
+
+# Boolean operations
+Union = namedtuple('Union', 'items')
+Difference = namedtuple('Difference', 'items')
+Intersection = namedtuple('Intersection', 'items')
+
+# Extrusions
+LinearExtrusion = namedtuple('LinearExtrusion', 'height center')
+RotateExtrusion = namedtuple('RotateExtrusion', 'twist')
